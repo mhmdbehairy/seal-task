380bacd4e9be44299646fc2a18468ced4b0f2673
Mohamed Elbehairy <mohamed.elbehairy@robustastudio.com>
2020-03-28 18:59:18 +0200

    feature: add app header and setup file structure

---
+++ src/components/header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import styled from '@emotion/styled/macro';
+import { Link } from 'react-router-dom';
+
+import { DeezerLogo } from 'components/svgs';
+
+const HeaderContainer = styled.header`
+  background-color: #3c3c3c;
+  padding: 15px 48px;
+
+  svg {
+    max-width: 100%;
+    height: auto;
+  }
+`;
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <DeezerLogo color={'#fff'} />
+      </Link>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/svgs/index.js
@@ -0,0 +1,1 @@
+export { default as DeezerLogo } from './components/logo';
