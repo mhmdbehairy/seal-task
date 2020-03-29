53c040f5b1c1178bc44f1b1b75cb0fb9c6da44b4
Mohamed Elbehairy <mohamed.elbehairy@robustastudio.com>
2020-03-29 16:51:39 +0200

    chore: renamed artist folder in components

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import AuthReducer from 'components/auth/authSlice';
 import GenreReducer from 'components/genre/genreSlice';
-import ArtistReducer from 'components/artist/artistSlice';
+import ArtistReducer from 'components/artists-list/artistSlice';
 
 export default configureStore({
   reducer: {
