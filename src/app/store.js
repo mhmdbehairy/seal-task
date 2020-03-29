ffa0105c085d0d89e4c6fcd8caf810fb48a83198
Mohamed Elbehairy <mohamed.elbehairy@robustastudio.com>
2020-03-29 15:26:04 +0200

    feature: fetch genres list

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import AuthReducer from 'components/auth/authSlice';
+import GenreReducer from 'components/genre/genreSlice';
 
 export default configureStore({
   reducer: {
-    authentication: AuthReducer
+    authentication: AuthReducer,
+    genre: GenreReducer
   }
 });
---
+++ src/components/genre/index.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import styled from '@emotion/styled/macro';
+import GenreCard from './genreCard';
+
+import { success, error, selectGenres } from 'components/genre/genreSlice';
+
+const { DZ } = window;
+
+const GenreContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-gap: 20px;
+  grid-auto-flow: row;
+`;
+
+const GenreGrid = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const genreList = useSelector(selectGenres);
+
+  useEffect(() => {
+    DZ.api('/genre', response => {
+      dispatch(success(response.data));
+    });
+  }, []);
+
+  console.log(genreList);
+
+  return (
+    <GenreContainer>
+      {genreList &&
+        genreList.map(genre => {
+          return (
+            <GenreCard
+              key={genre.id}
+              name={genre.name}
+              image={genre.picture}
+            ></GenreCard>
+          );
+        })}
+    </GenreContainer>
+  );
+};
+
+export default GenreGrid;
