1e0fb23f07f0c58f91e24a871d396f8606cfe1ce
Mohamed Elbehairy <mohamed.elbehairy@robustastudio.com>
2020-03-29 19:52:33 +0200

    feature: enhance artists list

--- src/components/artists-list/index.js
+++ src/components/artists-list/index.js
@@ -2,18 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
-import { Modal, Button } from 'antd';
+import { Modal, Button, List, Avatar } from 'antd';
 import styled from '@emotion/styled/macro';
 
 import { success, selectArtists } from './artistsSlice';
 
 const { DZ } = window;
 
-const ArtistsContainer = styled.div`
-  .ant-modal .ant-modal-content {
-    background-color: red;
-    height: 500px;
-  }
+const ArtistName = styled.p`
+  margin-bottom: 0px;
+  line-height: 35px;
 `;
 
 const ArtistList = () => {
@@ -33,27 +31,38 @@ const ArtistList = () => {
   };
 
   return (
-    <ArtistsContainer>
-      <Modal
-        visible={true}
-        title="Artists"
-        bodyStyle={{ height: '500px', overflowY: 'scroll' }}
-        onCancel={handleHide}
-        footer={[
-          <Button primary="true" key="back" onClick={handleHide}>
-            Return
-          </Button>
-        ]}
-      >
-        <ul>
-          {artistsList &&
-            artistsList.map(artist => {
-              return <li key={artist.id}>{artist.name}</li>;
-            })}
-        </ul>
-      </Modal>
-    </ArtistsContainer>
+    <Modal
+      visible={true}
+      title="Artists"
+      bodyStyle={{ height: '500px', overflowY: 'scroll' }}
+      onCancel={handleHide}
+      footer={[
+        <Button primary="true" key="back" onClick={handleHide}>
+          Return
+        </Button>
+      ]}
+    >
+      <List
+        itemLayout="horizontal"
+        dataSource={artistsList}
+        renderItem={item => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={<Avatar size="large" src={item.picture} />}
+              title={<ArtistName>{item.name}</ArtistName>}
+            />
+          </List.Item>
+        )}
+      />
+    </Modal>
   );
 };
 
 export default ArtistList;
+
+/* <ul>
+{artistsList &&
+  artistsList.map(artist => {
+    return <li key={artist.id}>{artist.name}</li>;
+  })}
+</ul> */
