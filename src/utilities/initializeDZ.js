ed3124eae62b4dd16e6b0f4e2058398c822ac09e
Mohamed Elbehairy <mohamed.elbehairy@robustastudio.com>
2020-03-31 15:00:29 +0200

    fix: channel url

--- src/utilities/initializeDZ.js
+++ src/utilities/initializeDZ.js
@@ -4,6 +4,9 @@ export const initDZ = () => {
   DZ.init({
     appId: '392944',
     secret: 'bdf0147e1fbda79d41721573c8d352c4',
-    channelUrl: process.env.REACT_APP_CHANNEL_URL
+    channelUrl:
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000/'
+        : 'https://5e833d7600cc04017644c1f1--inspiring-montalcini-0a8d4c.netlify.com/'
   });
 };
